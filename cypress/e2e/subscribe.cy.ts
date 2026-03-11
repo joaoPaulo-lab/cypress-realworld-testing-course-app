6b4efce53ab768a18bd0112f5eb0d02c535a2655
joaoPaulo-lab <72867987+joaoPaulo-lab@users.noreply.github.com>
2026-03-11 17:06:35 +0000

    Continuação de exercicios
    Validações de forms

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,9 +1,20 @@
 describe('check is user is subscribed',()=>{
+
     beforeEach(()=>{
         cy.visit('http://localhost:3000/')
         cy.get("#__next").should("be.visible")
     })
-    it('user is subscribed',()=>{
-        cy.getByData("email-input")
+
+    it('user can subscribe to application',()=>{
+        
+        cy.getByData("email-input").type("tom@aol.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("exist").contains("tom@aol.com")
+        
+    })
+    it.only('invalid e-mail address', ()=>{
+        cy.get('[data-test=email-input]').type('invalid')
+        cy.get('[data-test=submit-button]').click()
+        cy.get('[data-test=success-message]').should("not.exist")
     })
 })
