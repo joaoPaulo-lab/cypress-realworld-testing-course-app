b22e428fa2fbe5f02b78b8c049f5e7793852ecaf
joaoPaulo-lab <72867987+joaoPaulo-lab@users.noreply.github.com>
2026-03-12 15:03:56 +0000

    Exercicio com formularios e multiplas pag

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,30 +4,69 @@
 // first a string and then a callback function. Let’s update the string to the following:
 
 
-describe('enters website', () => {
+describe('Home page', () => {
+
   beforeEach(()=>{
     cy.visit('http://localhost:3000/')
     cy.get("#__next").should("be.visible")
      //Foi necessario adicionar para poder visualizar os elementos somente apos o site carregar
+
   })
 
-  it('the h1 contains the correct text', () => {
-    
-    //Foi necessario adicionar para poder visualizar os elementos somente apos o site carregar
+  context('check the text on page',()=>{
+
+  
+    it('the h1 contains the correct text', () => { 
+      //Foi necessario adicionar para poder visualizar os elementos somente apos o site carrega
+      cy.get('[data-test="hero-heading"]')
+      .contains('Cypress')
    
-    cy.get('[data-test="hero-heading"]')
-    .contains('Cypress')
-
-    
-  })
-  it("The features on the homepage are correct",()=>{
-      
-     
-      
-      cy.get('dt').eq(0).contains('4 Courses')
-      cy.get('dt').eq(1).contains('25+ Lessons')
-      cy.get('dt').eq(2).contains('Free and Open Source')
-      
-      
     })
+
+    it("The features on the homepage are correct",()=>{  
+        cy.get('dt').eq(0).contains('4 Courses')
+        cy.get('dt').eq(1).contains('25+ Lessons')
+        cy.get('dt').eq(2).contains('Free and Open Source')
+        
+      })
+
+  })
+  
+  context('Testing other pages',()=>{
+
+    it('course-0',()=>{
+      //O botão é o terceiro anchor do array
+      cy.get('[data-test=course-0]')
+      .find('a')
+      .eq(3)
+      .click()
+      
+      cy.location('pathname').should('eq','/testing-your-first-application')
+
+    })
+
+    it('course-1',()=>{
+      //O botão é o terceiro anchor do array
+      cy.get('[data-test=course-1]')
+      .find('a')
+      .eq(3)
+      .click()
+
+      cy.location('pathname').should('eq','/testing-foundations')
+
+    })
+
+    it('course-2',()=>{
+
+      cy.get('[data-test=course-2]')
+      .find('a')
+      .eq(3)
+      .click()
+
+      cy.location('pathname').should('eq','/cypress-fundamentals')
+
+    })
+
+  })
+
 })
--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -4,7 +4,7 @@ describe('check is user is subscribed',()=>{
         cy.visit('http://localhost:3000/')
         cy.get("#__next").should("be.visible")
     })
-
+    //Happy path, o utilizador consegue se inscrever
     it('user can subscribe to application',()=>{
         
         cy.getByData("email-input").type("tom@aol.com")
@@ -12,9 +12,17 @@ describe('check is user is subscribed',()=>{
         cy.getByData("success-message").should("exist").contains("tom@aol.com")
         
     })
-    it.only('invalid e-mail address', ()=>{
+    //Utilizador utiliza um e-mail invalido
+    it('invalid e-mail address', ()=>{
         cy.get('[data-test=email-input]').type('invalid')
         cy.get('[data-test=submit-button]').click()
         cy.get('[data-test=success-message]').should("not.exist")
+    })
+    //Checa se o utilzador já está inscrito na "bd"
+    it('User is already subscribed', ()=>{
+         cy.get('[data-test=email-input]').type('john@example.com')
+         cy.get('[data-test=submit-button]').click()
+         cy.get('[data-test=server-error-message]')
+         .contains('Error: john@example.com already exists. Please use a different email address.')
     })
 })
