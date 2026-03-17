7292e90acb893b82aa4a770dd44f96379349afdd
joaoPaulo-lab <72867987+joaoPaulo-lab@users.noreply.github.com>
2026-03-17 20:13:02 +0000

    aumentei o timeout e adicionei retry

--- cypress/e2e/userJourney.cy.ts
+++ cypress/e2e/userJourney.cy.ts
@@ -17,7 +17,7 @@ let selecionarRespostaProxPag = ()=>{
 } 
 
 let validaPath = (caminho)=>{
-    cy.location('pathname').should('eq',caminho)
+    cy.location('pathname').should('eq',caminho,{ retries: 3 })
 }
 
 //Para testar os cursos e necessario repetir muitas vezes este codigo, por isso criei funçoes para iterar sobre os cursos
